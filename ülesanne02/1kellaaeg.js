fd0a126c35bc4e385c2b86fb447f80862b1beab7
Mathias_M <144792835+MathMoell@users.noreply.github.com>
2026-01-27 18:19:09 +0200

    Create 1kellaaeg.js

---
+++ ülesanne02/1kellaaeg.js
@@ -0,0 +1,10 @@
+// Mathias Möll
+// Kuupäev: 27.01.2026
+// Ülessanne 2 (kellaaeg)
+
+let hr = 2;
+let min = 38;
+let sek = 58;
+let kellaaeg = `${hr}:${min}:${sek}PM`;
+
+console.log("Kellaaeg:", kellaaeg);
